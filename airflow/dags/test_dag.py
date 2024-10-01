eb1051b0924ecd03047c823535ff8a885236dd70
sserkanguzel <66708115+sserkanguzel@users.noreply.github.com>
2024-10-02 00:38:59 +0300

    first dag created for git sync

--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -1,1 +1,25 @@
-print('Hello world!')
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+# Define the DAG and set some default arguments
+default_args = {
+    'start_date': datetime(2023, 10, 1),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='hello_world_dag',
+    default_args=default_args,
+    schedule_interval='@daily',  # Runs daily
+    catchup=False,  # Do not backfill
+) as dag:
+
+    # Define the task using BashOperator
+    hello_world_task = BashOperator(
+        task_id='hello_world',
+        bash_command='echo "Hello, World!"'
+    )
+
+    # Set the task in the DAG
+    hello_world_task
